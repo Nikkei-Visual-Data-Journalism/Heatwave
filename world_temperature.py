7d0f4fbec2323a3043e98fff4eccffab0dcee80d
miotomita <mio.tomita@nex.nikkei.com>
2023-07-28 19:06:03 +0900

    change output directory to ./data/xxx.csv

--- world_temperature.py
+++ world_temperature.py
@@ -1,44 +1,17 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[38]:
-
-
 import requests
 import pandas as pd
 import numpy as np
 from datetime import datetime
 
-
-# In[39]:
-
-
 url = 'https://climatereanalyzer.org/clim/t2_daily/json/cfsr_world_t2_day.json'
-
-
-# In[40]:
-
 
 response = requests.get(url)
 
-
-# In[41]:
-
-
 data = response.json()
-
-
-# In[42]:
-
 
 df_data = pd.DataFrame(data)
 
-
-# In[44]:
-
-
 #flourishに読み込むデータ（df）はdate, year, tempで構成する
-
 today = datetime.today().strftime('%Y-%m-%d')
 
 dates = pd.date_range(start='1979-01-01', end=today)
@@ -55,97 +28,34 @@ df = pd.DataFrame({
     'year': year
 })
 
-
-# In[47]:
-
-
 #df_dataの下3行は使わないので削除する必要あり
-
 df_data = df_data.iloc[:-3]
-
-
-# In[49]:
-
 
 temp = []
 
-
-# In[50]:
-
-
 df_data.data[0]
 
-
-# In[52]:
-
-
 #閏年でない年は、最後にnoneが入っているから取り除く必要あり
-
 for i in range(len(df_data)):
     list_with_none = df_data.data[i]
     list_without_none = [x for x in list_with_none if x is not None]
-    
-#noneを取り除いたリストができたので、tempリストに集約（）
-    
+    #noneを取り除いたリストができたので、tempリストに集約（）    
     for k in range(len(list_without_none)):
         temp.append(round(list_without_none[k], 2))
 
-
-# In[53]:
-
-
 #本日の日付までデータが更新されているわけではないため、tempの数とdfの行数は異なる
 #なので足りない分、temp列をNaNで埋める
-
 df['temp'] = pd.Series(np.nan, index = df.index)
 
-
-# In[54]:
-
-
 #あるだけtempデータを入れる
-
 df.loc[df.index[:len(temp)], 'temp'] = temp
-
-
-# In[55]:
-
 
 df = df.dropna()
 
-
-# In[56]:
-
-
 df_without_229 = df[~df['date'].str.contains('^2/29$', regex=True)]
-
-
-# In[57]:
-
 
 df_without_229 = df_without_229[['temp', 'year', 'date']]
 
-
-# In[59]:
-
-
-df_without_229.to_csv("world_temperature.csv", index = False)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+#データ出力
+##dataフォルダにoutput
+df_without_229.to_csv("./data/world_temperature.csv", index = False)
