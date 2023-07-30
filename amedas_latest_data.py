863ccef094e4dde353fc518d3f7d4bcc0f4c17d6
miotomita <mio.tomita@nex.nikkei.com>
2023-07-30 23:34:56 +0900

    updates

--- amedas_latest_data.py
+++ amedas_latest_data.py
@@ -35,6 +35,18 @@ amedas[['lat','lon']] = amedas[['lat','lon']].applymap(lambda x: x[0] + x[1]/60)
 ##残りのリスト形式のデータから値のみ抽出
 amedas = amedas.applymap(lambda x: x[0] if type(x)==list else x)
 
+#色分け用
+amedas.loc[(amedas.temp<0),'temp_color'] = '氷点下'
+amedas.loc[(amedas.temp>=0)&(amedas.temp<5),'temp_color'] = '0〜5度以下'
+amedas.loc[(amedas.temp>=5)&(amedas.temp<10),'temp_color'] = '5〜10度以下'
+amedas.loc[(amedas.temp>=10)&(amedas.temp<15),'temp_color'] = '10〜15度以下'
+amedas.loc[(amedas.temp>=15)&(amedas.temp<20),'temp_color'] = '15〜20度以下'
+amedas.loc[(amedas.temp>=20)&(amedas.temp<25),'temp_color'] = '20〜25度以下'
+amedas.loc[(amedas.temp>=25)&(amedas.temp<30),'temp_color'] = '25〜30度以下'
+amedas.loc[(amedas.temp>=30)&(amedas.temp<35),'temp_color'] = '30〜35度以下'
+amedas.loc[(amedas.temp>=35)&(amedas.temp<40),'temp_color'] = '35〜40度以下'
+amedas.loc[(amedas.temp>=40),'temp_color'] = '40度以上'
+
 #データ出力
 ###全部入り
 amedas.to_csv('./data/amedas_latest_all.csv',encoding='utf-8-sig',index=False)
