c0ece5c41a82add76daa55040496ef07caaf8caa
hiroki-sakuragi <105629258+hiroki-sakuragi@users.noreply.github.com>
2023-08-03 11:13:27 +0900

    Update hot_index.py

--- hot_index.py
+++ hot_index.py
@@ -11,12 +11,7 @@ import ssl
 import urllib.request
 
 
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-    pass
-else:
-    ssl._create_default_https_context = _create_unverified_https_context
+ssl._create_default_https_context = ssl._create_unverified_context
 
 
 # In[40]:
