c28ad0b68ce774d0b80c997f05b696f9e56ec993
miotomita <mio.tomita@nex.nikkei.com>
2023-08-07 19:58:56 +0900

    updates

--- jma-maxtemp-01-get-data-past-7days.py
+++ jma-maxtemp-01-get-data-past-7days.py
@@ -7,6 +7,10 @@
 import pandas as pd
 import re
 
+#観測地点の一覧
+url = 'https://raw.githubusercontent.com/Nikkei-Visual-Data-Journalism/Heatwave/main/data-maxtemp/meta/points_list.csv'
+points = pd.read_csv(url)
+
 #データを取得してファイル名に日付を入れて保存する
 def get_previous_data(d):
     url = f'https://www.data.jma.go.jp/obd/stats/data/mdrr/tem_rct/alltable/mxtemsadext0{d}.csv'
@@ -26,7 +30,6 @@ def get_previous_data(d):
 #実行
 #集計（真夏・猛暑日を数える）
 data_agg = pd.DataFrame()
-
 ###データ取得
 for d in range(1, 8): 
     data = get_previous_data(d)
@@ -38,25 +41,66 @@ for d in range(1, 8):
     ##統合
     data_agg = pd.concat([data_agg, data])
 
-###計算
+###計算（集計1, 2共通）
 data_agg['over30'] = data_agg.maxtemp >= 30
 data_agg['over35'] = data_agg.maxtemp >= 35
 data_agg['over40'] = data_agg.maxtemp >= 40
 data_agg['total'] = 1
 data_agg['null_values'] = data_agg.maxtemp.isna()
+###追加データ（2で使用）
+data_agg['pref'] = data_agg['観測所番号'].map(points.set_index(['観測所番号']).pref.to_dict())
+data_agg['capitol'] = data_agg['観測所番号'].isin(points[points.capitol==1]['観測所番号']).astype(int)
 
-#集計df
+#【集計1】: 猛暑・真夏日の地点数の集計df（全国）
 heat_points_7days = data_agg.groupby(['date'])[['over30','over35','over40','total','null_values']].sum().reset_index()
-
-#過去分と統合
+###過去分と統合
 filename = "./data-maxtemp/timeseries-data/jma-maxtemp-heatpoints-ts.csv"
-##過去分を取得
+###過去分を取得
 heat_points = pd.read_csv(filename)
 heat_points = pd.concat([heat_points,heat_points_7days])
-##重複削除（新しく入ったほうを残す）
+###重複削除（新しく入ったほうを残す）
 heat_points.date = pd.to_datetime(heat_points.date)
 heat_points = heat_points.loc[~heat_points.duplicated(subset=['date'], keep='last')].reset_index(drop=True)
 heat_points = heat_points.dropna(subset='date').sort_values(by='date')
-
-##出力
+###出力
 heat_points.to_csv(filename, index=False)
+
+#【集計2】: 猛暑・真夏日の県別・日付別一覧
+####過去分（初回のみ）
+# import glob
+# files = glob.glob("./data-maxtemp/daily-data/jma-maxtemp-*.csv")
+# files = [file for file in files if 'latest' not in file]
+
+# past_df = pd.DataFrame()
+# for file in files:
+#     past_data = pd.read_csv(file)
+#     rename_dic = {col:'maxtemp' for col in past_data.columns if '日の最高気温(℃)' in col}
+#     past_data = past_data.rename(columns=rename_dic)
+#     past_data['date'] = pd.to_datetime(past_data.date)
+#     past_df = pd.concat([past_df, past_data])    
+# past_df['over30'] = past_df.maxtemp >= 30
+# past_df['over35'] = past_df.maxtemp >= 35
+# past_df['over40'] = past_df.maxtemp >= 40
+# past_df['pref'] = past_df['観測所番号'].map(points.set_index(['観測所番号']).pref.to_dict())
+# past_df['capitol'] = past_df['観測所番号'].isin(points[points.capitol==1]['観測所番号']).astype(int)
+##県別表示用一覧表
+# past_table = past_df.groupby(['date','pref'])[['maxtemp','over30','over35','over40']].max()
+# past_table_cap = past_df[past_df.capitol==1].set_index(['date','pref'])[['maxtemp','over30','over35','over40']].add_suffix('_capitol')
+# past_table = pd.concat([past_table, past_table_cap],axis=1).reset_index()
+##出力
+# filename = "./data-maxtemp/timeseries-data/jma-maxtemp-table-ts.csv"
+# past_table.to_csv(filename, index=False)
+
+###直近7日分
+data_table = data_agg.groupby(['date','pref'])[['maxtemp','over30','over35','over40']].max()
+data_table_cap = data_agg[data_agg.capitol==1].set_index(['date','pref'])[['maxtemp','over30','over35','over40']].add_suffix('_capitol')
+data_table = pd.concat([data_table, data_table_cap],axis=1).reset_index()
+###過去分
+filename = "./data-maxtemp/timeseries-data/jma-maxtemp-table-ts.csv"
+past_table = pd.read_csv(filename)
+past_table.date = pd.to_datetime(past_table.date)
+###統合
+data_table = pd.concat([past_table,data_table])
+data_table = data_table[~data_table.duplicated(subset=['date','pref'], keep='last')]
+###出力
+data_table.to_csv(filename, index=False)
