c6622bf61a485c3e34300118d8cd53dffe5d2c48
miotomita <mio.tomita@nex.nikkei.com>
2023-08-09 04:23:10 +0900

    updates

--- 04_tokyo_temperature_heatmap.py
+++ 04_tokyo_temperature_heatmap.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 # # 東京の夏の最高気温（ヒートマップ作成）
 # - github用
 # - 1963〜2022年（6月1日〜9月31日）の東京（東京都）の気温(データソース：気象庁「過去の気象データ・ダウンロード」https://www.data.jma.go.jp/risk/obsdl/index.php)
@@ -9,23 +6,12 @@
 # https://www.data.jma.go.jp/stats/etrn/view/daily_s1.php?prec_no=44&block_no=47662&year=2023&month=7 )
 # - 出力データ①（ヒートマップ用）：'tokyo_max_temp.csv'（東京過去70年分の最高気温）
 
-# In[16]:
-
-
 import pandas as pd
 import datetime
 from datetime import datetime
 
-
-# In[2]:
-
-
 # 現在の月を取得
 month = datetime.today().month
-
-
-# In[3]:
-
 
 # 気象庁から今年7月以降の気温データを読み込み
 
@@ -43,37 +29,17 @@ for i in range(7, month + 1):
     df['month'] = i
     df_list.append(df)
 
-
-# In[4]:
-
-
 # データフレームのリストを連結
 new_data = pd.concat(df_list)
-
-
-# In[5]:
-
 
 # 必要列を抽出
 new_data = new_data.loc[:, [('日', '日','日','日'), ('気温(℃)','気温(℃)', '最高', '最高'), ('month', '', '', '')]]
 
-
-# In[6]:
-
-
 # 欠損値がある行を削除
 new_data = new_data.dropna()
 
-
-# In[7]:
-
-
 # 列名を設定
 new_data.columns = ['day','max', 'month']
-
-
-# In[8]:
-
 
 # 年列を作成
 new_data['year'] = 2023
@@ -92,39 +58,19 @@ new_data['month_day'] = new_data['date'].dt.strftime('%m-%d')
 # date列を文字列に戻す
 new_data['date'] = new_data['date'].astype(str)
 
-
-# In[9]:
-
-
 new_data = new_data[['date','max','year','month','day','month_day']]
 
 
 # ### 過去データ（1964〜2022年の6月1日〜9月31日と2023年6月分）と最新データを結合
 
-# In[10]:
-
-
 # 過去データ読み込み
-past_data = pd.read_csv('1963_2022_all_202306only.csv')
-
-
-# In[11]:
-
+past_data = pd.read_csv('./data-tokyo/1963_2022_all_202306only.csv')
 
 # 過去データと最新データを結合
 data = pd.concat([past_data, new_data], ignore_index=True)
 
-
-# In[12]:
-
-
 # flourish用に日付データを作成
 data['month_day_flourish'] = data['month'].astype(str) + '/' + data['day'].astype(str)
 
-
-# In[13]:
-
-
 # 保存
-data.to_csv('data/tokyo_max_temp.csv')
-
+data.to_csv('./data-tokyo/tokyo_max_temp.csv')
