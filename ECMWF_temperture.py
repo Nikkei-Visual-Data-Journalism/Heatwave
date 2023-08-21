5ec1b96abb79ae52b59ace1ba744442b98693970
Kento Tanaka <137042134+kentokenobi@users.noreply.github.com>
2023-08-21 09:53:57 +0900

    ECMWF_temperture.py

--- ECMWF_temperture.py
+++ ECMWF_temperture.py
@@ -21,8 +21,8 @@ filename = 'medium-z500-t850.grib'
 
 #dateで0を基準にマイナスをセットすると過去データとれる。最新データ
 client.retrieve(
-    date=-1,
-    time=0,
+    #date=-1,
+    #time=0,
     step=12,
     stream="oper",
     type="fc",
