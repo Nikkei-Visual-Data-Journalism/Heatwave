7775e0c3a963bf6cffb9f35cef9cf94c598aad7c
miotomita <mio.tomita@nex.nikkei.com>
2023-08-24 09:17:31 +0900

    update

--- not-in-use/wbgt.py
+++ not-in-use/wbgt.py
@@ -20,16 +20,15 @@ wbgt = pd.read_csv(url)
 wbgt['Time'] = wbgt['Time'].str.replace('24:00','0:00')
 wbgt['date_dt'] = pd.to_datetime(wbgt['Date'] + ' ' + wbgt['Time'], format='%Y/%m/%d %H:%M')
 wbgt['dt_popup']=wbgt.date_dt.dt.strftime('%-m月%-d日 %-H時時点')
-wbgt = wbgt.drop(['Date','Time'],axis=1)
+#wbgt = wbgt.drop(['Date','Time'],axis=1)
 
 #longフォーマットに転換
-wbgt = wbgt.set_index(['date_dt','dt_popup']).rename_axis('amdno',axis=1).stack().rename('wbgt').reset_index()
+wbgt = wbgt.set_index(['Date','Time','dt_popup','date_dt']).rename_axis('amdno',axis=1).stack().rename('wbgt').reset_index()
 wbgt.amdno = wbgt.amdno.astype(int)
 
 #最新分のみ切り出し
 ##iloc[-1]などはほぼ大丈夫ですが、絶対値で指定したほうがより安全
 wbgt_latest = wbgt.loc[wbgt.date_dt==wbgt.date_dt.max()]
-
 
 #地点情報を取得
 ##tableを固定で引っ張ると、観測地点の変更・追加などがあるとデータがおちてしまうので、
@@ -41,12 +40,14 @@ data = r.json()
 locations = pd.DataFrame.from_dict(data, orient='index').reset_index()
 locations = locations.rename(columns={'index':'amdno'})
 locations.amdno = locations.amdno.astype(int)
+
 #lat longを整える
-locations[['lat','lon']] = locations[['lat','lon']].applymap(lambda x: x[0] + x[1]/60)
+#Flourishではlongになっていたので
+locations[['lat','long']] = locations[['lat','lon']].applymap(lambda x: x[0] + x[1]/60)
 
 #地点情報を最新データに追加
 ##how='left'
-wbgt_latest = wbgt_latest.merge(locations[['amdno','lat','lon','kjName']], on='amdno', how='left')
+wbgt_latest = wbgt_latest.merge(locations[['amdno','lat','long','kjName']], on='amdno', how='left')
 
 #暑さ指数「日常生活に関する指針」
 ##指針の内容をテキストでアサイン
@@ -63,6 +64,7 @@ def guideline(value):
         return 'ほぼ安全'
 wbgt_latest['guideline'] = wbgt_latest.wbgt.apply(guideline)
 
+#並び順をFlourish用にそろえる
 cols = ['Date','Time','amdno','wbgt','dt_popup','lat','long','kjName','guideline']
 wbgt_latest = wbgt_latest.loc[:,cols]
 
